5ccef28071c527ecbdaf81c0bb97903c7033fb72
Hayat <rahnamoonhayatullah@gmail.com>
2018-03-30 13:14:13 +0800

    pagination has updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Pagination Demo';
+  p: number = 2;
+  collection: any[] = []; 
+
+  constructor(){ 
+
+  }
+  ngOnInit(){
+    for (let i = 0; i <50; i++) {
+      this.collection.push("line no "+i)
+    }
+  }
 }
